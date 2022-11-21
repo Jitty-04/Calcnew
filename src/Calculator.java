453351d23c53dfdcb9cb1d27be13dc6d4071c766
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-21 20:37:09 +0530

    p2

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,17 @@
+import java.util.*;
 public class Calculator {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        double a = sc.nextInt();
+        System.out.println("Enter second number");
+        double b = sc.nextInt();
+        System.out.println("1-Addition");
+        System.out.println("2-Substraction");
+        System.out.println("3-Multiplication");
+        System.out.println("4-Division");
+        System.out.println("5-Exit");
+        int c = sc.nextInt();
 
+    }
 }
